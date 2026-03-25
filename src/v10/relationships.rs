a4070b7ddb2b9629185bc1420dabc74465cd182e
Luuk van Oijen <lazyluuk.channel@gmail.com>
2026-03-26 00:22:00 +0100

    create relationship

--- src/v10/relationships.rs
+++ src/v10/relationships.rs
@@ -1,8 +1,5 @@
 use crate::v10::V10Impl;
-use yapspace_protocol::v10::{
-    channel::Channel,
-    user::{Relationship, SendFriendRequest},
-};
+use yapspace_protocol::v10::user::{CreateRelationship, Relationship, SendFriendRequest};
 
 impl<'a> V10Impl<'a> {
     pub async fn get_relationships(&self) -> Result<Vec<Relationship>, reqwest::Error> {
@@ -15,6 +12,19 @@ impl<'a> V10Impl<'a> {
     ) -> Result<(), reqwest::Error> {
         self.0
             .post(&["users", "@me", "relationships"], friend_request)
+            .await
+    }
+
+    pub async fn create_relationship(
+        &self,
+        user_id: &str,
+        create_relationship: &CreateRelationship,
+    ) -> Result<(), reqwest::Error> {
+        self.0
+            .post(
+                &["users", "@me", "relationships", user_id],
+                create_relationship,
+            )
             .await
     }
 }
